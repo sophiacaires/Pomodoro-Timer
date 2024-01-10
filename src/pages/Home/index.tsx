205fcd44aa6b9044a571ff82c8a07c8be6dd7bd2
Sophia <soph.caires@gmail.com>
2024-01-10 18:07:43 -0300

    countdown and title changes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,8 @@ import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { differenceInSeconds } from 'date-fns'
 
 import {
   CountdownContainer,
@@ -14,12 +15,36 @@ import {
   TaskInput,
 } from './styles'
 
+// REGISTER
+
 // register(name: string){} - função que devolve vários métodos que permitem definir cada um dos métodos em uma propriedade do input
 // register(name, {value type})
+
+// USE EFFECT
+
+// toda vez que há alteração no item monitorado (dependências), ocorre o efeito colateral
+// também executa toda vez que o componente é reendenizado, na primeira vez em que é exibido
+// useEffect(() => {
+//   efeito colateral
+// }, [itens a serem monitorados]) -> array de dependências
+
+// se nao houver nenhuma dependência ele será executado apenas uma vez, bom para chamar API
+// exemplo de código chamando uma API e preenchendo a lista com a os usuarios
+// useEffect(() => {
+//   fetch('https://api.github.com/users/sophiacaires/repos')
+//     .then((response) => response.json())
+//     .then((data) => {
+//       setList(data.mao((item: any) => item.full_name))
+//     })
+// }, [])
+
+// dificilmente o useEffect será usado para atualizar um estado - evitando muitas reendenizações
+// como para filtrar uma lista que já esta sendo exibida, nao é necessario utilizar mais um useState, pois ja temos as informações disponiveis em um lista anterior
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
   minutesAmount: zod.number().min(5).max(60),
+  startDate: zod.date(),
 })
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
@@ -28,6 +53,7 @@ interface Cycle {
   id: string
   task: string
   minutesAmount: number
+  startDate: Date
 }
 
 export function Home() {
@@ -40,8 +66,27 @@ export function Home() {
     defaultValues: {
       task: '',
       minutesAmount: 0,
+      startDate: new Date(),
     },
   })
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  useEffect(() => {
+    let interval: number
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate),
+        )
+      }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [activeCycle])
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime())
@@ -50,6 +95,7 @@ export function Home() {
       id,
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startDate: data.startDate,
     }
 
     setCycles((state) => [...state, newCycle])
@@ -57,8 +103,6 @@ export function Home() {
 
     reset()
   }
-
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
@@ -68,6 +112,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [activeCycle, minutes, seconds])
 
   const task = watch('task')
   const isSubmitDisabled = !task
